15d3b6122697bc1c30fc4e3f406a2d6e9cd94601
MahirKothari <64363014+MahirKothari@users.noreply.github.com>
2021-03-09 18:00:19 +0530

    Add files via upload

---
+++ screens/SettingsScreen.js
@@ -0,0 +1,18 @@
+import React,{Component}from 'react';
+import {
+    View,
+    Text,
+    TextInput,
+    Modal,
+    KeyboardAvoidingView,
+    StyleSheet,
+    TouchableOpacity,
+    Alert,
+    ScrollView} from 'react-native';
+
+
+import db from '../Config';
+import firebase from 'firebase';
+import MyHeader from '../components/MyHeader';
+import { Touchable } from 'react-native';
+
